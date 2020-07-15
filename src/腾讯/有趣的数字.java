27756e9e07ea23f7c05f4bc6c2d521d999ed0353
liuxiaolinnanyou <2248822970@qq.com>
2020-07-15 16:50:44 +0800

    增加有趣的数字

--- src/腾讯/有趣的数字.java
+++ src/腾讯/有趣的数字.java
@@ -1,5 +1,7 @@
 package 腾讯;
 
+import java.util.*;
+
 /**
  * 小Q今天在上厕所时想到了这个问题：有n个数，两两组成二元组，相差最小的有多少对呢？相差最大呢？
  * <p>
@@ -30,6 +32,56 @@ public class 有趣的数字 {
     // 1. 如果最小差为0，则为 每组相等数中 任选2个的所有 组合 数。
     // 2. 如果最小差不为0，则为 相邻数差值 = 最小差 的所有 数量。
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+            Arrays.sort(a);
+            //如果数组中的值全相同，直接两两组合
+            if (a[0] == a[n - 1]) {
+                int tmp = (n * (n - 1)) / 2;
+                System.out.println(tmp + " " + tmp);
+                continue;
+            }
+            //map用来统计
+            Map<Integer, Integer> map = new TreeMap<>();
+            for (int i = 0; i < n; i++) {
+                if (map.containsKey(a[i]))
+                    map.put(a[i], map.get(a[i]) + 1);
+                else
+                    map.put(a[i], 1);
+            }
+            //求差最小个数
+            int minres = 0;
+            if (map.size() == n) {
+                int min = Math.abs(a[1] - a[0]);
+                for (int i = 2; i < n; i++) {
+                    int tmp = Math.abs(a[i] - a[i - 1]);
+                    if (tmp == min)
+                        minres++;
+                    else if (tmp < min) {
+                        min = tmp;
+                        minres = 1;
+                    }
+                }
+            } else {
+                for (Integer key : map.keySet()) {
+                    int val = map.get(key);
+                    if (val > 1) {
+                        minres += (val * (val - 1)) / 2;
+                    }
+                }
+            }
+            //求差最大个数
+            int maxres = 0;
+            List<Integer> keyset = new ArrayList<>(map.keySet());
+            int val1 = map.get(keyset.get(0));
+            int val2 = map.get(keyset.get(keyset.size() - 1));
+            maxres = val1 * val2;
+            System.out.println(minres + " " + maxres);
+        }
     }
 }
