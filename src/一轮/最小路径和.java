16a52c6502140a68204e10bd072cf695fdcf28fa
liuxiaolinnanyou <2248822970@qq.com>
2020-07-22 08:42:03 +0800

    7.22

--- src/一轮/最小路径和.java
+++ src/一轮/最小路径和.java
@@ -44,19 +44,19 @@ public class 最小路径和 {
     // dp(i,j)=grid(i,j)+min(dp(i+1,j),dp(i,j+1))
     public static int minPathSum2(int[][] grid) {
 //        int[][] dp = new int[grid.length][grid[0].length];
-////        for (int i = grid.length - 1; i >= 0; i--) {
-////            for (int j = grid[0].length - 1; j >= 0; j--) {
-////                if (i == grid.length - 1 && j != grid[0].length - 1)
-////                    dp[i][j] = grid[i][j] + dp[i][j + 1];
-////                else if (j == grid[0].length - 1 && i != grid.length - 1)
-////                    dp[i][j] = grid[i][j] + dp[i + 1][j];
-////                else if (j != grid[0].length - 1 && i != grid.length - 1)
-////                    dp[i][j] = grid[i][j] + Math.min(dp[i + 1][j], dp[i][j + 1]);
-////                else
-////                    dp[i][j] = grid[i][j];
-////            }
-////        }
-////        return dp[0][0];
+//        for (int i = grid.length - 1; i >= 0; i--) {
+//            for (int j = grid[0].length - 1; j >= 0; j--) {
+//                if (i == grid.length - 1 && j != grid[0].length - 1)
+//                    dp[i][j] = grid[i][j] + dp[i][j + 1];
+//                else if (j == grid[0].length - 1 && i != grid.length - 1)
+//                    dp[i][j] = grid[i][j] + dp[i + 1][j];
+//                else if (j != grid[0].length - 1 && i != grid.length - 1)
+//                    dp[i][j] = grid[i][j] + Math.min(dp[i + 1][j], dp[i][j + 1]);
+//                else
+//                    dp[i][j] = grid[i][j];
+//            }
+//        }
+//        return dp[0][0];
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
