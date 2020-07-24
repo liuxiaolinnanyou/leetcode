2da527ddc90b9f4612fffb143044afbfb8a3fc96
liuxiaolinnanyou <2248822970@qq.com>
2020-07-24 21:03:07 +0800

    7.24

--- src/华为/Hw102_字符统计.java
+++ src/华为/Hw102_字符统计.java
@@ -29,7 +29,7 @@ public class Hw102_字符统计 {
             int[] mat = new int[128];
             for (int i = 0; i < str.length(); i++) {
                 if ((str.charAt(i) >= 'a' && str.charAt(i) <= 'z') || (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') || (str.charAt(i) >= '0' && str.charAt(i) <= '9' || str.charAt(i) == ' ')) {
-                    int pos = str.charAt(i);
+                    int pos = str.charAt(i);    // 可以将字符类型直接复制给 int
                     mat[pos]++;
                 }
             }
