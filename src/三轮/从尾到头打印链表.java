dfb3276682ef3841eedba35fd14f5b2effd78eb2
liuxiaolinnanyou <2248822970@qq.com>
2020-07-27 13:45:25 +0800

    7.27

---
+++ src/三轮/从尾到头打印链表.java
@@ -0,0 +1,31 @@
+package 三轮;
+
+import java.util.Stack;
+
+/**
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ * 输入：head = [1,3,2]
+ * 输出：[2,3,1]
+ * <p>
+ * 0 <= 链表长度 <= 10000
+ */
+public class 从尾到头打印链表 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] reversePrint(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+        ListNode temp = head;
+        while (temp != null) {
+            stack.push(temp);
+            temp = temp.next;
+        }
+        int size = stack.size();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = stack.pop().val;
+        }
+        return arr;
+    }
+}
