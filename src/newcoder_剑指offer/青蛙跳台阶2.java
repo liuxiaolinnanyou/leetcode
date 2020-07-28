40244a0355c61824d9e0faee5c646bab91e37988
liuxiaolinnanyou <2248822970@qq.com>
2020-07-28 09:27:06 +0800

    7.28

--- src/newcoder_剑指offer/青蛙跳台阶2.java
+++ src/newcoder_剑指offer/青蛙跳台阶2.java
@@ -1,5 +1,8 @@
 package newcoder_剑指offer;
 
+/**
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ */
 public class 青蛙跳台阶2 {
     public static void main(String[] args) {
         System.out.println(jumpFloor(2));
