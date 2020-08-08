d4df4201db1bc9f56766474704d7418853471561
liuxiaolinnanyou <2248822970@qq.com>
2020-08-08 09:40:01 +0800

    8.8

--- src/面试宝典_字符串/字符串的反转.java
+++ src/面试宝典_字符串/字符串的反转.java
@@ -1,5 +1,9 @@
 package 面试宝典_字符串;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * 把一个句子中的单词进行反转，例如："how are you",反转后变成"you are how"。
  */
@@ -7,6 +11,7 @@ public class 字符串的反转 {
     public static void main(String[] args) {
         String s = "how are you";
         System.out.println(swapWords(s));
+        System.out.println(swapWords2(s));
 //        System.out.println("--------------------------");
 //        StringBuilder sb = new StringBuilder();
 //        for (int i = s.length() - 1; i >= 0; i--) {
@@ -43,5 +48,16 @@ public class 字符串的反转 {
             front++;
             end--;
         }
+    }
+
+    // 自己写的
+    public static String swapWords2(String s) {
+        String[] split = s.trim().split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = split.length - 1; i >= 0; i--) {
+            sb.append(split[i]).append(" ");
+        }
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
 }
