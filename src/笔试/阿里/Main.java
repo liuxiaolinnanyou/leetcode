93434f1dcda08dcc64fa10f9d7eb4d16e975f104
liuxiaolinnanyou <2248822970@qq.com>
2020-08-10 20:36:36 +0800

    8.10

--- src/笔试/阿里/Main.java
+++ src/笔试/阿里/Main.java
@@ -3,6 +3,21 @@ package 笔试.阿里;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * 一条直线道路上有 N 棵树，现要在这条道路上某个位置修建水塔，要求这 N 棵树所在的位置坐标到水塔的位置坐标总距离最短，
+ * 请求出水塔位置坐标。(所有位置坐标均为整数)
+ * <p>
+ * 输入描述：
+ * 第一行一个整数 N 。
+ * 第二行共 N 个整数a1,a2,...aN表示 N 棵树的坐标。
+ * 2 <= N <= 5*10^5   ai < 2^31
+ * <p>
+ * 输入：
+ * 4
+ * 0 1 4 6
+ * 输出：
+ * 9
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -24,7 +39,6 @@ public class Main {
             sum += Math.abs(arr[index] - arr[i]);
         }
         System.out.println(sum);
-
     }
 }
 //    Scanner sc = new Scanner(System.in);
@@ -42,7 +56,6 @@ public class Main {
 //        min = Math.min(min, sum);
 //        }
 //        System.out.println(min);
-
 
 
 //    Scanner sc = new Scanner(System.in);
