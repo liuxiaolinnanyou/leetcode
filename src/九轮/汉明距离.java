014476f8cd564d28d8a0fbbe36265267b437ef34
liuxiaolinnanyou <2248822970@qq.com>
2020-08-11 12:06:02 +0800

    8.11

---
+++ src/九轮/汉明距离.java
@@ -0,0 +1,26 @@
+package 九轮;
+
+/**
+ * 两个整数之间的汉明距离指的是这两个数字对应二进制位不同的位置的数目。
+ * 给出两个整数 x 和 y，计算它们之间的汉明距离。
+ * 注意：
+ * 0 ≤ x, y < 231.
+ * <p>
+ * 输入: x = 1, y = 4
+ * 输出: 2
+ */
+public class 汉明距离 {
+    public static void main(String[] args) {
+        System.out.println(hammingDistance(1, 4));
+    }
+
+    public static int hammingDistance(int x, int y) {
+        int n = x ^ y;
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
+}
