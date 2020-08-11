1b25982cf6e2fbb581d20d275629cef182e4ad41
liuxiaolinnanyou <2248822970@qq.com>
2020-08-11 12:01:22 +0800

    8.11

--- src/九轮/汉明距离总和.java
+++ src/九轮/汉明距离总和.java
@@ -1,5 +1,7 @@
 package 九轮;
 
+import java.util.Arrays;
+
 /**
  * 两个整数的 汉明距离 指的是这两个数字的二进制数对应位不同的数量。
  * 计算一个数组中，任意两个数之间汉明距离的总和。
@@ -17,12 +19,16 @@ package 九轮;
 public class 汉明距离总和 {
     public static void main(String[] args) {
         int[] arr = {4, 14, 2};
-        System.out.println(totalHammingDistance(arr));
+//        System.out.println(totalHammingDistance(arr));
+        System.out.println("------------------------");
+        System.out.println(totalHammingDistance2(arr));
     }
 
     // 位运算
     // 求数组汉明距离，我们可以每次遍历数组的一位上有多少0和多少1，
     // 该位的汉明距离的和就是 0 的个数 × 1 的个数，因为 0 和 0 ，1 和 1 的汉明距离都是 0。然后遍历 32 位，全部加起来即可。
+
+    // 这题可不可以将数组中的数转成对应的二进制字符串表示    然后统计二进制字符串中各个位上 1 的个数和 0 的个数
     public static int totalHammingDistance(int[] nums) {
         if (nums.length <= 1) {
             return 0;
@@ -31,7 +37,7 @@ public class 汉明距离总和 {
         int[] sums = new int[32];
         for (int i = 0; i < len; i++) {           // 统计每个二进制位上的1出现个数
             for (int j = 0; j < 32; j++) {
-                sums[j] += nums[i] & 1;
+                sums[j] += nums[i] & 1;           // 看是否是 1
                 nums[i] = nums[i] >> 1;
                 if (nums[i] == 0) {
                     break;
@@ -43,5 +49,32 @@ public class 汉明距离总和 {
             sum += sums[i] * (len - sums[i]);
         }
         return sum;
+    }
+
+    public static int totalHammingDistance2(int[] nums) {
+        int len = nums.length;
+        String[] str = new String[len];
+        for (int i = 0; i < len; i++) {
+            str[i] = Integer.toBinaryString(nums[i]);
+        }
+//        System.out.println(Arrays.toString(str));         // [100, 1110, 10]
+        int maxLength = 0;
+        for (int i = 0; i < len; i++) {
+            maxLength = Math.max(maxLength, str[i].length());
+        }
+        int[] sum = new int[maxLength];
+        for (int i = 0; i < len; i++) {   // 统计 1 的个数
+            for (int j = 0; j < str[i].length(); j++) {
+                if (str[i].charAt(j) == '1') {
+                    sum[maxLength - str[i].length() + j]++;
+                }
+            }
+        }
+//        System.out.println(Arrays.toString(sum));
+        int ans = 0;
+        for (int i = 0; i < maxLength; i++) {
+            ans += sum[i] * (len - sum[i]);
+        }
+        return ans;
     }
 }
