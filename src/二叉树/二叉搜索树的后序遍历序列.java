b6267351708de47fb064f86fe2fd6bfa90b134a6
liuxiaolinnanyou <2248822970@qq.com>
2020-08-26 16:57:18 +0800

    8.26

--- src/二叉树/二叉搜索树的后序遍历序列.java
+++ src/二叉树/二叉搜索树的后序遍历序列.java
@@ -5,6 +5,9 @@ package 二叉树;
  * 如果是则返回true,否则返回false。假设输入的数组的任意两个数字都互不相同。
  * <p>
  * {3,4,9,5,12,11,10}
+ * <p>
+ * 一颗 BST：左孩子 < 根节点 < 右孩子
+ * 一颗 BST 的左子树或者右子树都是 BST
  */
 public class 二叉搜索树的后序遍历序列 {
     public static void main(String[] args) {
