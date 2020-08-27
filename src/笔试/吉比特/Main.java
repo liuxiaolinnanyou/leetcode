84b4759fb857aec60a1658e44050b70619848061
liuxiaolinnanyou <2248822970@qq.com>
2020-08-27 22:29:24 +0800

    8.27

--- src/笔试/吉比特/Main.java
+++ src/笔试/吉比特/Main.java
@@ -1,7 +1,50 @@
 package 笔试.吉比特;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * 给定一个由英文字母组成的字符串，假定字符 c1 和 字符 c2 在字符串中的位置分别为 p1 和 p2，求 p1 和 p2 差的绝对值的最大值。
+ *
+ * 输入只有一行，依次为一个由英文字母组成的字符串、字符1和字符2
+ * 字符串长度不超过64 ，字母不区分大小写
+ *
+ * 输出一个整数，为字符最大距离，如果给定字符不在字符串中，则输出 -1 即可。
+ *
+ * 输入：
+ * ba A b
+ * 输出：
+ * 1
+ */
+// AC 90
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String s = sc.nextLine();
+            String[] split = s.split(" ");
+            String str = split[0].toLowerCase();
+            char c1 = Character.toLowerCase(split[1].toCharArray()[0]);
+            char c2 = Character.toLowerCase(split[2].toCharArray()[0]);
 
+            if (c1 == c2) {
+                System.out.println(0);
+                return;
+            }else if (!str.contains("" + c1) || !str.contains("" + c2)) {
+                System.out.println(-1);
+                return;
+            }
+            ArrayList<Integer> list1 = new ArrayList<>();
+            ArrayList<Integer> list2 = new ArrayList<>();
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == c1) {
+                    list1.add(i);
+                }
+                if (str.charAt(i) == c2) {
+                    list2.add(i);
+                }
+            }
+            System.out.println(Math.abs(list1.get(0) - list2.get(list2.size() - 1)));
+        }
     }
 }
--- src/笔试/吉比特/Main1.java
+++ src/笔试/吉比特/Main1.java
@@ -1,7 +1,53 @@
 package 笔试.吉比特;
 
-public class Main1 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+/**
+ * 1 1 1 1
+ * 1 1
+ * 1 1
+ * 1 1
+ * 3 3 3
+ * <p>
+ * 1 2 3 1
+ * 4 5
+ * 6 7
+ * 8 9
+ * 37 33 22
+ */
+public class Main1 {
+    // AC 0
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//        int n = sc.nextInt();
+        String s = sc.nextLine();
+        String[] split = s.split(" ");
+        long a = Long.parseLong(split[0]);
+        long b = Long.parseLong(split[1]);
+        long c = Long.parseLong(split[2]);
+        long n = Long.parseLong(split[3]);
+
+        String[] strArr = new String[3];
+        for (int i = 0; i < 3; i++) {
+            strArr[i] = sc.nextLine();
+        }
+
+        long[][] arr = new long[3][2];
+        for (int i = 0; i < 3; i++) {
+            arr[i][0] = Long.parseLong(strArr[i].split(" ")[0]);
+            arr[i][1] = Long.parseLong(strArr[i].split(" ")[1]);
+        }
+        long sumA = a;
+        long sumB = b;
+        long sumC = c;
+        for (int i = 1; i <= n; i++) {
+            sumA += (b * arr[1][0] + c * arr[2][0]);
+            sumB += (a * arr[0][0] + c * arr[2][1]);
+            sumC += (a * arr[0][1] + b * arr[1][1]);
+        }
+        System.out.println(sumA + " " + sumB + " " + sumC);
     }
 }
