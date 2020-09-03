e2fdc9e86327bbd42032149b0a1883b24de3e6ed
liuxiaolinnanyou <2248822970@qq.com>
2020-09-03 18:01:05 +0800

    9.3

---
+++ src/sort_排序算法/归并排序.java
@@ -0,0 +1,49 @@
+package sort_排序算法;
+
+import java.util.Arrays;
+
+public class 归并排序 {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 2, 6, 1, 3};
+        System.out.println(Arrays.toString(arr));
+        mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void mergeSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        mergeSort(arr, 0, arr.length - 1);
+    }
+
+    // 递归调用
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l == r) {
+            return;
+        }
+        int mid = l + ((r - l) >> 1);
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    public static void merge(int[] arr, int l, int m, int r) {
+        int[] help = new int[r - l + 1];
+        int i = 0;
+        int p1 = l;
+        int p2 = m + 1;
+        while (p1 <= m && p2 <= r) {
+            help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= m) {
+            help[i++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[i++] = arr[p2++];
+        }
+        for (i = 0; i < help.length; i++) {
+            arr[l + i] = help[i];
+        }
+    }
+}
