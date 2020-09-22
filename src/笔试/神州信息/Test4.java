382879d2bdba811e8de8a504a6d722735a837950
liuxiaolinnanyou <2248822970@qq.com>
2020-09-22 20:44:46 +0800

    9.22

--- src/笔试/神州信息/Test4.java
+++ src/笔试/神州信息/Test4.java
@@ -2,18 +2,18 @@ package 笔试.神州信息;
 
 public class Test4 {
     public static void throwit() {
-        System.out.print("throwit");
+        System.out.print("throwit ");
         throw new RuntimeException();
     }
 
     public static void main(String[] args) {
         try {
-            System.out.print("hello");
+            System.out.print("hello ");
             throwit();
         } catch (Exception e) {
-            System.out.print("caught");
+            System.out.print("caught ");
         } finally {
-            System.out.print("finally");
+            System.out.print("finally ");
         }
         System.out.print("after");
     }
