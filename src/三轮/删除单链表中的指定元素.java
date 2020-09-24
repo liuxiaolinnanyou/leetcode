3adbece25eb80fd390383449da3a7de592ec26b8
liuxiaolinnanyou <2248822970@qq.com>
2020-09-24 10:13:18 +0800

    9.24

--- src/三轮/删除单链表中的指定元素.java
+++ src/三轮/删除单链表中的指定元素.java
@@ -1,5 +1,12 @@
 package 三轮;
 
+/**
+ * 1.判断头结点是否为需要删除的元素，为防止链表开头有多个元素均为指定元素，所以使用while循环判断
+ * 2.判断头结点是否为空，
+ * 3.声明一个prev节点，prev.next节点为当前判断的节点，
+ * 如果当前节点的值等于指定元素，那么直接用prev.next.next替换prev.next；
+ * 如果不相等，则执行prev = prev.next，判断的下一个节点;
+ */
 public class 删除单链表中的指定元素 {
     public static void main(String[] args) {
         ListNode a = new ListNode(1);
