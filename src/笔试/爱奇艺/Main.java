71671387d0ee1fe0e56cc0d39c5af2ae62797c23
liuxiaolinnanyou <2248822970@qq.com>
2020-10-19 16:16:09 +0800

    10.19

--- src/笔试/爱奇艺/Main.java
+++ src/笔试/爱奇艺/Main.java
@@ -2,6 +2,9 @@ package 笔试.爱奇艺;
 
 import java.util.Scanner;
 
+/**
+ * 计算 n 的阶乘中尾部所包含零的个数
+ */
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
