b5e4a0b245ec9d4cb0555d211d112a739f83b946
liuxiaolinnanyou <2248822970@qq.com>
2020-11-02 21:51:10 +0800

    11.2

--- src/LeetCode_双指针/分割回文串.java
+++ src/LeetCode_双指针/分割回文串.java
@@ -35,13 +35,6 @@ public class 分割回文串 {
         return res;
     }
 
-    /**
-     * @param s
-     * @param start 起始字符的索引
-     * @param len   字符串 s 的长度，可以设置为全局变量
-     * @param path  记录从根结点到叶子结点的路径
-     * @param res   记录所有的结果
-     */
     private static void backtracking(String s, int start, int len, Deque<String> path, List<List<String>> res) {
         if (start == len) {     // 到达字符串末尾
             res.add(new ArrayList<>(path));
@@ -59,14 +52,6 @@ public class 分割回文串 {
         }
     }
 
-    /**
-     * 这一步的时间复杂度是 O(N)，因此，可以采用动态规划先把回文子串的结果记录在一个表格里
-     *
-     * @param str
-     * @param left  子串的左边界，可以取到
-     * @param right 子串的右边界，可以取到
-     * @return
-     */
     private static boolean checkPalindrome(String str, int left, int right) {
         // 严格小于即可
         while (left < right) {
