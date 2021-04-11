3ecc3465d7f83d1a692f8a0546648e1760c7feb4
liuxiaolinnanyou <2248822970@qq.com>
2021-04-11 21:32:10 +0800

    2021.4.11

--- src/笔试/字节跳动春招/Main.java
+++ src/笔试/字节跳动春招/Main.java
@@ -1,7 +1,72 @@
 package 笔试.字节跳动春招;
 
+// AC 100
+/**
+ * 破译
+ * 1.字母仅包含大写字母，每个字母（A-Z），都可能会表示另一个字母
+ * 2.每个数字（0-9），都可能会表示另一个数字
+ * 3.除了数字与字母以外的字符，不会发生变化
+ * <p>
+ * 破译与情报的第一个字符x密切相关
+ * 1.若x为字母，则情报中的所有字母和数字均按照字母表顺序往后x位
+ * 2.若x为数字，则情报中的所有字母和数字均按照字母表顺序往后x位
+ * 3.若x为其他字符，则与x为A等价
+ * <p>
+ * 如果在替换字母的过程中，向后移动x位，超过了字母Z，则会回到A开始计算。
+ * 如果在替换数字的过程中，向后移动x位，超过了字母9，则会回到0开始计算。
+ * <p>
+ * 输入：
+ * Lx.91
+ * 输出：
+ * XJ.13
+ * <p>
+ * 输入：
+ * %123
+ * 输出：
+ * %234
+ */
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
 
+        StringBuilder sb = new StringBuilder();
+
+        char ch;
+        int x;
+        if (Character.isLetter(s.charAt(0))) {
+            ch = s.charAt(0);
+            x = ch - 'A' + 1;
+        } else if (Character.isDigit(s.charAt(0))) {
+            ch = s.charAt(0);
+            x = ch - '0';
+        } else {
+            x = 1;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetterOrDigit(s.charAt(i))) {
+                int add = s.charAt(i) + x;
+                if (Character.isLetter(s.charAt(i))) {
+                    if (add >= 65 && add <= 90) {
+                        sb.append((char) (s.charAt(i) + x));
+                    } else {
+                        sb.append((char) (s.charAt(i) + x - 26));
+                    }
+                } else {
+                    if (add >= 48 && add <= 57) {
+                        sb.append((char) (s.charAt(i) + x));
+                    } else {
+                        sb.append((char) ((Integer.parseInt("" + s.charAt(i)) + x) % 10 + 48));
+                    }
+                }
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        System.out.println(sb.toString());
     }
 }
--- src/笔试/字节跳动春招/Main1.java
+++ src/笔试/字节跳动春招/Main1.java
@@ -1,7 +1,61 @@
 package 笔试.字节跳动春招;
+// AC 80
+
+/**
+ * 一个服务会维护两个字符串s和m，调用方会带着两个参数来请求，分别是i和j，i代表着s串的前i个字符的子串
+ * j代表着m串的前j个字符的子串，需要查询这两个子串组合起来去重后有多少不同的字符。现在有t个请求。
+ * <p>
+ * 输入描述：
+ * 首先两个字符串（长度小于等于1000，只有小写字母字符串），然后是请求次数t，表示会发起t次查询（t<=1000000）
+ * 接下来t行数据，分别为i，j（i和j都是合法数字，即小于等于字符串长度）
+ * 输出描述：
+ * 每组数据输出t次查询的结果
+ * <p>
+ * 输入：
+ * ab
+ * ac
+ * 3
+ * 1 1
+ * 1 2
+ * 2 2
+ * 输出：
+ * 1
+ * 2
+ * 3
+ */
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Main1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String m = sc.nextLine();
 
+        int n = Integer.parseInt(sc.nextLine());
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (n > 0) {
+            String line = sc.nextLine();
+            String[] split = line.split(" ");
+            int i = Integer.parseInt(split[0]);
+            int j = Integer.parseInt(split[1]);
+            String s1 = s.substring(0, i);
+            String s2 = m.substring(0, j);
+            String ss = s1 + s2;
+            Set<Character> set = new HashSet<>();
+
+            for (char c : ss.toCharArray()) {
+                set.add(c);
+            }
+            list.add(set.size());
+            n--;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
     }
 }
