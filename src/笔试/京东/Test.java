f9e9242f835c4fd14e931ab88485fe6a6bec40f3
liuxiaolinnanyou <2248822970@qq.com>
2021-04-11 10:12:54 +0800

    2021.4.11

--- src/笔试/京东/Test.java
+++ src/笔试/京东/Test.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-// 姚凌峰写的
 public class Test {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
