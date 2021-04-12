acfdc53401b3d6831a8c596ca1510323fc5e484b
liuxiaolinnanyou <2248822970@qq.com>
2021-04-12 18:56:28 +0800

    2021.4.12

--- src/Test/StringArrayTest2.java
+++ src/Test/StringArrayTest2.java
@@ -26,6 +26,7 @@ public class StringArrayTest2 {
         // 注意比较两者的区别
 //        Arrays.sort(arr, (o1, o2) -> o1[0].equals(o2[0]) ? o1[1].compareTo(o2[1]) : o1[0].compareTo(o2[0]));
         Arrays.sort(arr, (o1, o2) -> o1[0].equals(o2[0]) ? Integer.parseInt(o1[1]) - Integer.parseInt(o2[1]) : o1[0].compareTo(o2[0]));
+
         for (String[] str : arr) {
             System.out.println(Arrays.toString(str));
         }
