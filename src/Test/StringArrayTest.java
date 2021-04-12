e39f535509bc3f8dd5b98b9e33f015905c044731
liuxiaolinnanyou <2248822970@qq.com>
2021-04-12 18:52:35 +0800

    2021.4.12

---
+++ src/Test/StringArrayTest.java
@@ -0,0 +1,19 @@
+package Test;
+
+import java.util.Arrays;
+
+public class StringArrayTest {
+    public static void main(String[] args) {
+        String[][] arr = {{"hello", "world"}, {"apple", "phone"}, {"oppo", "phone"}, {"apple", "car"}};
+        for (String[] str : arr) {
+            System.out.println(Arrays.toString(str));
+        }
+
+        System.out.println("------------------------------------------------");
+        Arrays.sort(arr, (o1, o2) -> o1[0].equals(o2[0]) ? o1[1].compareTo(o2[1]) : o1[0].compareTo(o2[0]));
+
+        for (String[] str : arr) {
+            System.out.println(Arrays.toString(str));
+        }
+    }
+}
---
+++ src/Test/StringArrayTest2.java
@@ -0,0 +1,31 @@
+package Test;
+
+// 测试荣耀的笔试题
+
+import java.util.Arrays;
+
+public class StringArrayTest2 {
+    public static void main(String[] args) {
+        String[][] arr = {{"Apple", "62"},
+                {"Apple", "73"},
+                {"Orange", "65"},
+                {"Orange", "90"},
+                {"Apple", "91"},
+                {"Orange", "88"},
+                {"Orange", "90"},
+                {"Apple", "100"}};
+
+        for (String[] str : arr) {
+            System.out.println(Arrays.toString(str));
+        }
+
+        System.out.println("----------------------------");
+
+        // 注意比较两者的区别
+//        Arrays.sort(arr, (o1, o2) -> o1[0].equals(o2[0]) ? o1[1].compareTo(o2[1]) : o1[0].compareTo(o2[0]));
+        Arrays.sort(arr, (o1, o2) -> o1[0].equals(o2[0]) ? Integer.parseInt(o1[1]) - Integer.parseInt(o2[1]) : o1[0].compareTo(o2[0]));
+        for (String[] str : arr) {
+            System.out.println(Arrays.toString(str));
+        }
+    }
+}
