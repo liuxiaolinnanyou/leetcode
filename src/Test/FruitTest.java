d1796e7328108b9c2d20c88db820e29c58b8dfc5
liuxiaolinnanyou <2248822970@qq.com>
2021-04-13 13:43:21 +0800

    2021.4.13

--- src/Test/FruitTest.java
+++ src/Test/FruitTest.java
@@ -8,10 +8,10 @@ public class FruitTest {
         Fruit f2 = new Fruit("Apple", 2, 62);
         Fruit f3 = new Fruit("Apple", 4, 73);
         Fruit f4 = new Fruit("Orange", 4, 65);
-        Fruit f5 = new Fruit("Orange", 1, 90);
+        Fruit f5 = new Fruit("Orange", 5, 90);
         Fruit f6 = new Fruit("Apple", 3, 91);
         Fruit f7 = new Fruit("Orange", 3, 88);
-        Fruit f8 = new Fruit("Orange", 5, 90);
+        Fruit f8 = new Fruit("Orange", 1, 90);
 
         Fruit[] arr = {f1, f2, f3, f4, f5, f6, f7, f8};
         for (Fruit fruit : arr) {
