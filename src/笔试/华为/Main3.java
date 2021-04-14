5fec8d55c26c2300beea6ed0705c0087c5642fa2
liuxiaolinnanyou <2248822970@qq.com>
2021-04-14 20:16:23 +0800

    2021.4.14

--- src/笔试/华为/Main3.java
+++ src/笔试/华为/Main3.java
@@ -34,10 +34,11 @@ public class Main3 {
                 reverse(arr, stack.pop() + 1, i - 1);
             }
         }
-        for (int i = 0; i < arr.length; i++)
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] != ')' && arr[i] != '(') {
                 sb.append(arr[i]);
             }
+        }
         return sb.toString();
     }
 
