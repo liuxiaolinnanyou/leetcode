858dfa8971b9c3424ea2570d590a6e7b52108f93
liuxiaolinnanyou <2248822970@qq.com>
2021-04-15 11:38:38 +0800

    2021.4.15

---
+++ src/Test/值传递问题.java
@@ -0,0 +1,43 @@
+package Test;
+
+public class 值传递问题 {
+    public static void main(String[] args) {
+        Student s1 = new Student("小张");
+        Student s2 = new Student("小李");
+        swap(s1, s2);
+        System.out.println("s1:" + s1.getName());
+        System.out.println("s2:" + s2.getName());
+    }
+
+    public static void swap(Student x, Student y) {
+        Student temp = x;
+        x = y;
+        y = temp;
+        System.out.println("x:" + x.getName());
+        System.out.println("y:" + y.getName());
+    }
+}
+
+
+class Student {
+    String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
