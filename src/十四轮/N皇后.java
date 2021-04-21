c195b0a97cd2d05ecb66243f35b5891b1a48fda2
liuxiaolinnanyou <2248822970@qq.com>
2021-04-21 20:54:20 +0800

    2021.4.21

---
+++ src/十四轮/N皇后.java
@@ -0,0 +1,86 @@
+package 十四轮;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// 力扣 第 51 题
+
+/**
+ * n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ * 输入：n = 4
+ * 输出：[[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+ * 解释：如上图所示，4 皇后问题存在两个不同的解法。
+ * <p>
+ * 输入：n = 1
+ * 输出：[["Q"]]
+ * <p>
+ * 说明：
+ * 1 <= n <= 9
+ * 皇后彼此不能相互攻击，也就是说：任何两个皇后都不能处于同一条横行、纵行或斜线上。
+ */
+public class N皇后 {
+    public static void main(String[] args) {
+        System.out.println(solveNQueens(4));
+    }
+
+    // 回溯法
+    public static List<List<String>> solveNQueens(int n) {
+        char[][] chess = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                chess[i][j] = '.';
+            }
+        }
+        List<List<String>> res = new ArrayList<>();
+        solve(res, chess, 0);
+        return res;
+    }
+
+    public static void solve(List<List<String>> res, char[][] chess, int row) {
+        if (row == chess.length) {
+            res.add(construct(chess));
+            return;
+        }
+        for (int col = 0; col < chess.length; col++) {
+            if (valid(chess, row, col)) {
+                chess[row][col] = 'Q';
+                solve(res, chess, row + 1);
+                chess[row][col] = '.';
+            }
+        }
+    }
+
+    // row表示第几行，col表示第几列    用于验证是否可以在该位置放置皇后
+    public static boolean valid(char[][] chess, int row, int col) {
+        // 判断当前列有没有皇后,因为他是一行一行往下走的，
+        // 我们只需要检查走过的行数即可，通俗一点就是判断当前
+        // 坐标位置的上面有没有皇后
+        for (int i = 0; i < row; i++) {
+            if (chess[i][col] == 'Q') {
+                return false;
+            }
+        }
+        // 判断当前坐标的右上角有没有皇后
+        for (int i = row - 1, j = col + 1; i >= 0 && j < chess.length; i--, j++) {
+            if (chess[i][j] == 'Q') {
+                return false;
+            }
+        }
+        // 判断当前坐标的左上角有没有皇后
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (chess[i][j] == 'Q') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 把数组转为list
+    public static List<String> construct(char[][] chess) {
+        List<String> path = new ArrayList<>();
+        for (int i = 0; i < chess.length; i++) {
+            path.add(new String(chess[i]));
+        }
+        return path;
+    }
+}
