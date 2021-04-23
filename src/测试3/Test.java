e293ffaaf96721cc53585ed8e8011993570bad27
liuxiaolinnanyou <2248822970@qq.com>
2021-04-23 10:59:25 +0800

    2021.4.23

--- src/测试3/Test.java
+++ src/测试3/Test.java
@@ -9,13 +9,13 @@ public class Test {
         for (short i = 0; i < 100; i++) {
             s.add(i);
             s.remove(i - 1);            // 这边注意：set 集合中添加的元素是 Short 类型的    而移出的是 int 类型的
-                                           // 如果改成 i      set 集合的长度就会变成 0
+            // 如果改成 i      set 集合的长度就会变成 0
         }
         System.out.println(s.size());
         System.out.println(s);
 
 //        Set<Short> set = new HashSet<>();
-//        set.add((short)1);
+//        set.add((short) 1);
 //        set.remove((short) 1);
 //        System.out.println(set);
     }
