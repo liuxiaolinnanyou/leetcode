b0df303ae7f25845b3259292475c31bcea043808
liuxiaolinnanyou <2248822970@qq.com>
2021-05-12 16:19:10 +0800

    2021.5.12

--- src/笔试/五八同城/Main1.java
+++ src/笔试/五八同城/Main1.java
@@ -1,13 +1,23 @@
 package 笔试.五八同城;
 
+// 实现层序遍历
 import java.util.ArrayList;
 
 public class Main1 {
     public static void main(String[] args) {
-
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n3 = new TreeNode(3);
+        TreeNode n4 = new TreeNode(4);
+        TreeNode n5 = new TreeNode(5);
+        n1.left = n2;
+        n1.right = n3;
+        n2.left = n4;
+        n2.right = n5;
+        System.out.println(printNode(n1));
     }
 
-    public ArrayList<ArrayList<Integer>> printNode(TreeNode node) {
+    public static ArrayList<ArrayList<Integer>> printNode(TreeNode node) {
         // write code here
         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
         ArrayList<TreeNode> queue = new ArrayList<>();
---
+++ src/笔试/携程/Main.java
@@ -0,0 +1,7 @@
+package 笔试.携程;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
