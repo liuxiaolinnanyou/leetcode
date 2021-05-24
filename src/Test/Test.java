160fc8a58a619826e34878bbafd7b5b60a3e0612
liuxiaolinnanyou <2248822970@qq.com>
2021-05-24 10:15:45 +0800

    2021.5.24

---
+++ src/Test/Test.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class Test {
+    public static void main(String[] args) {
+        boolean flag1 = (4 * 0.1) == 0.4;
+        boolean flag2 = (3 * 0.1) == 0.3;
+        System.out.println(flag1);
+        System.out.println(flag2);
+        System.out.println("--------------------------");
+        System.out.println(4 * 0.1);
+        System.out.println(3 * 0.1);
+    }
+}
